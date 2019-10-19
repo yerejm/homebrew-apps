59e272f5ea92ce087c03d7c239b2f5378a732cf3
yerejm <github@thelostyerejm.com>
2019-10-20 00:26:07 +1000

    si 1.0.0

--- Casks/si.rb
+++ Casks/si.rb
@@ -1,8 +1,8 @@
 cask 'si' do
-  version :latest
-  sha256 :no_check
+  version '1.0.0'
+  sha256 '3b2afe29ed49c6a4e257a7ee7e221af7e848d34cf0f7379a49abe49b816c7c06'
 
-  url 'https://gitlab.com/yerejm/releases/uploads/52efb8e94d8eb282c1bf3e23806229d9/si-macos.zip'
+  url 'https://gitlab.com/yerejm/releases/uploads/100bc82fdab0961b545367e589c8411b/si-1.0.0-macos.dmg'
   name 'si'
   homepage 'https://yerejm.com/'
 
